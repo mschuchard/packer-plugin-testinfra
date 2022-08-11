bb47b4120be99759fa0d261cc803b1044353eee8
Matt Schuchard <grapheneowns@gmail.com>
2022-08-11 10:18:36 -0400

    expand unit tests

--- packer-provisioner-testinfra_test.go
+++ packer-provisioner-testinfra_test.go
@@ -12,6 +12,10 @@ var basicTestinfraConfig = &TestinfraConfig{
   TestFile:   "fixtures/test.py",
 }
 
+var minTestinfraConfig = &TestinfraConfig{
+  TestFile: "fixtures/test.py",
+}
+
 // test config for packer template/config data
 func TestProvisionerConfig(test *testing.T) {
   var provisioner = &TestinfraProvisioner{
@@ -31,12 +35,54 @@ func TestProvisionerInterface(test *testing.T) {
   }
 }
 
-// test provisioner prepare
+// test provisioner prepare with basic config
 func TestProvisionerPrepareBasic(test *testing.T) {
   var provisioner TestinfraProvisioner
 
   err := provisioner.Prepare(basicTestinfraConfig)
   if err != nil {
     test.Errorf("Prepare function failed with basic Testinfra Packer config")
+  }
+}
+
+// test provisioner prepare with minimal config
+func TestProvisionerPrepareMinimal(test *testing.T) {
+  var provisioner TestinfraProvisioner
+
+  err := provisioner.Prepare(minTestinfraConfig)
+  if err != nil {
+    test.Errorf("Prepare function failed with minimal Testinfra Packer config")
+  }
+
+  if provisioner.config.PytestPath != "py.test" {
+    test.Errorf("Default setting for PytestPath is incorrect: %s", provisioner.config.PytestPath)
+  }
+}
+
+// test provisioner prepare errors on nonexistent pytest
+func TestProvisionerPrepareNoPytest(test *testing.T) {
+  var provisioner TestinfraProvisioner
+  var noPytestTestinfraConfig = &TestinfraConfig{
+    PytestPath: "/home/foo/py.test",
+    TestFile:   "fixtures/test.py",
+  }
+
+  err := provisioner.Prepare(noPytestTestinfraConfig)
+  if err == nil {
+    test.Errorf("Prepare function did not fail on nonexistent pytest")
+  }
+}
+
+// test provisioner prepare errors on nonexistent test file
+func TestProvisionerPrepareNoTestFile(test *testing.T) {
+  var provisioner TestinfraProvisioner
+  var noTestFileTestinfraConfig = &TestinfraConfig{
+    PytestPath: "/usr/local/bin/py.test",
+    TestFile:   "/home/foo/test.py",
+  }
+
+  err := provisioner.Prepare(noTestFileTestinfraConfig)
+  if err == nil {
+    test.Errorf("Prepare function did not fail on nonexistent testfile")
   }
 }
