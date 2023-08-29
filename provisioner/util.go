c3403e01ae710b9f150811ed60154cd85a109667
Matt Schuchard <grapheneowns@gmail.com>
2023-08-29 09:37:05 -0400

    add unit test for uploadFiles

--- provisioner/util.go
+++ provisioner/util.go
@@ -12,29 +12,33 @@ import (
 
 // helper function to transfer files from local device to temporary packer instance
 func (provisioner *Provisioner) uploadFiles(comm packer.Communicator, files []string, destDir string) error {
+	var err error
+
 	// iterate through files to transfer
 	for _, file := range files {
 		// validate file existence
-		if _, err := os.Stat(file); errors.Is(err, os.ErrNotExist) {
-			log.Printf("The file does not exist at: %s, and will not be transferred", file)
+		if _, err = os.Stat(file); errors.Is(err, os.ErrNotExist) {
+			log.Printf("the file does not exist at path: %s, and will not be transferred", file)
 			continue
 		}
 
 		// determine file content (io.Reader) from file path (string)
-		fileBytes, err := os.ReadFile(file)
-		if err != nil {
-			log.Printf("the file at %s could not be read", file)
-			return err
+		var fileBytes []byte
+		if fileBytes, err = os.ReadFile(file); err != nil {
+			log.Printf("the file at path '%s' could not be read, and will not be transferred", file)
+			continue
 		}
 		fileIo := bytes.NewReader(fileBytes)
 
 		// upload file to destination dir
 		destination := fmt.Sprintf("%s/%s", destDir, file)
 		if err = comm.Upload(destination, fileIo, nil); err != nil {
-			log.Printf("The file at %s could not be transferred to %s on the temporary Packer instance", file, destination)
-			return err
+			log.Printf("the file at %s could not be transferred to %s on the temporary Packer instance", file, destination)
+			continue
 		}
 	}
 
-	return nil
+	// return most recent error
+	// the logger displays the relevant debugging information, and this return is useful only in a nil comparable context, and not for specific error types
+	return err
 }
--- provisioner/util_test.go
+++ provisioner/util_test.go
@@ -1,7 +1,23 @@
 package testinfra
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/packer-plugin-sdk/packer"
+)
 
 func TestProvisionerUploadFiles(test *testing.T) {
+	var provisioner Provisioner
+	comm := new(packer.MockCommunicator)
 
+	if emptyReturn := provisioner.uploadFiles(comm, []string{}, "/tmp/"); emptyReturn != nil {
+		test.Errorf("uploadFiles with empty files slice did not return no error, but instead %s was returned", emptyReturn)
+	}
+
+	err := provisioner.uploadFiles(comm, []string{"foobar"}, "/tmp/")
+	if !errors.Is(err, os.ErrNotExist) {
+		test.Errorf("expected nonexistent file to return ErrNotExist error, but instead %s was returned", err)
+	}
 }
