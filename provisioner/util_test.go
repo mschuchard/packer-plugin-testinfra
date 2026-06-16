9af1e492e051688052c711b7231344b158d4a241
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2026-06-16 10:10:26 -0400

    add unit test for redact utility

--- provisioner/util_test.go
+++ provisioner/util_test.go
@@ -3,6 +3,7 @@ package testinfra
 import (
 	"errors"
 	"os"
+	"slices"
 	"testing"
 
 	"github.com/hashicorp/packer-plugin-sdk/packer"
@@ -51,5 +52,15 @@ func TestProvisionerUploadFiles(test *testing.T) {
 	err = uploadFiles(comm, []string{"foobar"}, "/tmp")
 	if !errors.Is(err, os.ErrNotExist) {
 		test.Errorf("expected nonexistent file to return ErrNotExist error, but instead %s was returned", err)
+	}
+}
+
+func TestRedact(test *testing.T) {
+	expected := []string{"--hosts=ssh://user:REDACTED@10.0.0.1", "some", "--hosts=winrm://admin:REDACTED@server", "--some-flag", "value", "--hosts=ssh://user:REDACTED@host.com"}
+	redacted := redact([]string{"--hosts=ssh://user:pass@10.0.0.1", "some", "--hosts=winrm://admin:secret@server", "--some-flag", "value", "--hosts=ssh://user:p@ss!word@host.com"})
+
+	if !slices.Equal(redacted, expected) {
+		test.Error("input string slice was not redacted correctly")
+		test.Errorf("expected: %q+, actual: %q+", expected, redacted)
 	}
 }
